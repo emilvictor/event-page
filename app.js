39fa77697ac4050b0625741d26e6d2ff3b146754
Emil Pettersson <61893425+emilvictor@users.noreply.github.com>
2021-03-15 15:24:05 +0100

    hamburger menu done

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ window.addEventListener("load", () => {
   const preloader = document.querySelector(".preloader");
   preloader.classList.add("preloader-finish");
 });
+
+function hamburgerToggle() {
+  const hamburgerMenu = document.querySelector(".dropdown");
+  hamburgerMenu.classList.toggle("on");
+  console.log("Hello");
+};
